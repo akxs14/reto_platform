0f282dce3c0230989475c02706f0f9c21a2ef0a5
akxs14 <angelos.kapsimanis@madvertise.com>
2014-05-04 00:26:52 +0200

    Barebones sinatra app

---
+++ app.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'sinatra'
+require 'slim'
+
+get '/' do
+  redirect '/campaigns'
+end
+
+get '/campaigns' do
+  slim :campaigns
+end
+
+get '/javascripts/applications.js' do
+  coffee :application
+end
+
+
